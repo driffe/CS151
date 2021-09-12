faa2c5a3ce1bdfc3cba61e797e4fbef769ec7c05
Seyoung <seyoung2k@naver.com>
2021-09-12 16:42:16 -0700

    Modify

--- src/HW01.java
+++ src/HW01.java
@@ -5,10 +5,6 @@ public class HW01 {
 
     }
     public static void main(String[] args) {
-        HW01 test = new HW01();
-        test.number = 1;
-        int num01 = test.number;
 
-        System.out.println(num01);
     }
 }
