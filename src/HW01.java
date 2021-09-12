28fa6906b0a53f0c511b1a3c3555cb207670e0eb
Seyoung <seyoung2k@naver.com>
2021-09-12 16:14:03 -0700

    add one instance and constructor

---
+++ src/HW01.java
@@ -0,0 +1,14 @@
+public class HW01 {
+    public int number;
+
+    public HW01() {
+
+    }
+    public static void main(String[] args) {
+        HW01 test = new HW01();
+        test.number = 1;
+        int num01 = test.number;
+
+        System.out.println(num01);
+    }
+}
