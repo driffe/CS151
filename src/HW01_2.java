42769f1dcae0d9889dfa750adc6884f661f66616
Seyoung <seyoung2k@naver.com>
2021-09-12 17:23:22 -0700

    Create a class for the object and print

--- src/HW01_2.java
+++ src/HW01_2.java
@@ -6,7 +6,7 @@ public class HW01_2 {
     public static void main(String[] args) {
         HW01 hw = new HW01(20); // Create the object of HW01 and give 20 to instance variable
 
-        int hwNum = hw.number;
+        int hwNum = hw.number; //get hw.number with hwNum
 
         System.out.println(hwNum); // Print
     }
