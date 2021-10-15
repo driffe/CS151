6b02ca6fdf2c8f4c64703990644882459273f33a
Seyoung <seyoung2k@naver.com>
2021-10-15 03:25:00 -0700

    my files are gone so add agian

---
+++ src/HW02/ArrayListUtilsTest.java
@@ -0,0 +1,20 @@
+package HW02;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+public class ArrayListUtilsTest {
+    @Test
+    public void testSum() {
+        ArrayListUtils arrayListUtils = new ArrayListUtils();
+        ArrayList<Integer> arr01 = new ArrayList<Integer>();
+        int testArrSum;
+        arr01.add(1);
+        arr01.add(2);
+        arr01.add(3);
+        testArrSum = ArrayListUtils.sum(arr01);
+        assertEquals(true, arrayListUtils.sum(arr01) == testArrSum);
+    }
+}
