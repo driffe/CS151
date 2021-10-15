a0694ef309bab6aecf34d1e22f86aa978ae5ee6f
Seyoung <seyoung2k@naver.com>
2021-10-15 02:56:49 -0700

    Add function(sum) that total sum of all elements

---
+++ src/HW02/ArrayListUtils.java
@@ -0,0 +1,35 @@
+package HW02;
+
+import java.util.ArrayList;
+
+/**
+ * Class that provides helper functions for ArrayList
+ */
+public class ArrayListUtils {
+
+    /**
+     * Calculates total sum of all elements in the given array
+     *
+     * @param arr array list of integers for which we calculate sum
+     * @return the total sum of all elements in the array
+     */
+    public static int sum(ArrayList<Integer> arr) {
+        // TODO: implement this method on a separate branch. Should return sum of all elements in the array list
+        int resultSum = 0;
+        for(int i : arr) {
+            resultSum = resultSum + i;
+        }
+        return resultSum;
+    }
+
+    /**
+     * Calculates average value of all elements in the given array
+     *
+     * @param arr array list of integers for which we calculate average
+     * @return average value of all elements in the array
+     */
+    public static int mean(ArrayList<Integer> arr) {
+        // TODO: implement this method on a separate branch. Should return the average of elements in the array list
+        return 0;
+    }
+}
