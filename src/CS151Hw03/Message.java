28105161851cdb85071cf32240c73a89740ea7ab
SeyoungOh <33598679+driffe@users.noreply.github.com>
2021-12-07 20:54:31 -0800

    Delete src/CS151Hw03 directory

--- src/CS151Hw03/Message.java
+++
@@ -1,9 +0,0 @@
-package CS151Hw03;
-
-import java.io.Serializable;
-
-/**
- * interface of message
- */
-public interface Message extends Serializable {
-}
--- src/CS151Hw03/ResetMessage.java
+++
@@ -1,7 +0,0 @@
-package CS151Hw03;
-
-/**
- * class for resetting the message
- */
-public class ResetMessage implements Message {
-}
